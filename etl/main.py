0cf08ca815a6da33372315b7cca4ade828e8d615
Ivan Camilo Rosales <rivancamilo@gmail.com>
2025-02-01 11:03:23 -0500

    Prueba de lectura del archivo XML

--- etl/main.py
+++ etl/main.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import xml.etree.ElementTree as et
+
+
 print('Hola, Mundo Docker!')
+
+arbol = et.parse('./data/feed.xml') 
+data2 = arbol.findall('listing') 
+
+for registro, indice in zip(data2, range(1, 6)): 
+    name = registro.find('state').text 
+    id = registro.find('city').text 
+    department = registro.find('colony').text
+    
+    
+    print(f'los valores de i:{registro} - j:{indice} - valor: {name}')
+    
