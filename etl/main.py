82cfdddec06f7944e47937b3cee01843056f6cfe
Ivan Camilo Rosales <rivancamilo@gmail.com>
2025-02-02 09:38:52 -0500

    Creamos la imagen de Docker y un script Python base para pruebas

--- etl/main.py
+++ etl/main.py
@@ -1,17 +1,33 @@
 import pandas as pd
 import xml.etree.ElementTree as et
-
+import os
 
 print('Hola, Mundo Docker!')
 
-arbol = et.parse('./data/feed.xml') 
+arbol = et.parse('./source/feed.xml') 
 data2 = arbol.findall('listing') 
+datos = []
 
 for registro, indice in zip(data2, range(1, 6)): 
-    name = registro.find('state').text 
-    id = registro.find('city').text 
-    department = registro.find('colony').text
+    estado = registro.find('state').text 
+    ciudad = registro.find('city').text 
+    colonia = registro.find('colony').text
+    
+    datos.append({'Estado':estado,
+                  'Ciudad':ciudad,
+                  'Colonia':colonia})
     
     
-    print(f'los valores de i:{registro} - j:{indice} - valor: {name}')
+    print(f'los valores de i:{indice} - registro:{registro} - valor: {estado}')
     
+#creamos un dataframe
+df = pd.DataFrame(datos)
+
+#exportanos el dataFrame
+df.to_csv('./target/datos.csv', sep='|', index=False, encoding='utf-8')
+    
+    
+directorio = "./target"
+contenido = os.listdir(directorio)
+print(contenido)
+                       
