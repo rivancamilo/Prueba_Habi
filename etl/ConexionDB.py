938643a791eae74074ce0226b5cb123467e33d70
Ivan Camilo Rosales <rivancamilo@gmail.com>
2025-02-02 10:13:21 -0500

    creamos la clase de ConexionDB.py con el método constructor

---
+++ etl/ConexionDB.py
@@ -0,0 +1,36 @@
+## importamos las librerías
+
+import mysql.connector
+from mysql.connector import errorcode
+
+class ConexionDB:
+    
+    # ------------------------------------------------------------------
+    # ------------------------------------------------------------------
+    # Definimos el método constructor con los parámetros 
+    # iniciales para establecer una conexión con Mysql
+    def __init__(self, USER, PASS, HOST, DATABASE):
+        self.USER = USER
+        self.PASS = PASS
+        self.HOST = HOST
+        self.DATABASE = DATABASE
+        self.conn = None
+        
+        try:
+            self.conn = mysql.connector.connect(
+                user=self.USER,
+                password=self.PASS,
+                host=self.HOST,
+                database=self.DATABASE
+            )
+            self.conn.autocommit = False
+            print(f"Estamos conectados a la Base de datos: {self.DATABASE}")
+            
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Error el usuario o la contraseña son incorrectos")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print(f"La BD {self.DATABASE} a la que se está conectando NO existe")
+            else:
+                print(err)
+                
