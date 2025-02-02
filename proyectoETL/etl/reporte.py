38fb40cfe03543d022ca4fb20a264152426c79a3
Ivan Camilo Rosales <rivancamilo@gmail.com>
2025-02-02 10:44:15 -0500

    Empezamos a configurar los archivos del proyecto

---
+++ proyectoETL/etl/reporte.py
@@ -0,0 +1,15 @@
+from etl.ConexionDB import *
+import os
+import pandas as pd
+from decouple import config
+
+# ------------------------------------------------------------------
+# ------------------------------------------------------------------
+# Creamos una instancia de la clase ConexiónDB
+
+conexion = ConexionDB( USER = config('DB_USER')
+                        , PASS = config('DB_PASSWORD_ROOT')
+                        , HOST = config('DB_HOST')
+                        , DATABASE = config('DB_NAME') )
+
+print(conexion)
---
+++ proyectoETL/main.py
@@ -0,0 +1,13 @@
+from etl.preprocesamiento import extraccion_xml
+
+def main():
+    rutaSource = './source/feed.xml'
+    rutaTarget = './target/'
+    
+    print('Hola, Mundo Docker!')
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
